5f6848a067c973371bfb7e9f9548f655bfb54e2c
laynechensquare <laynechensquare@gmail.com>
2025-01-11 14:43:57 +0800

    feat: update tailwind.config.ts for reusability

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,9 +8,30 @@ export default {
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      fontFamily: {
+        primary: ["proxima-nova", "sans-serif"],
+      },
+      textColor: {
+        primary: "var(--color-font-primary)",
+        secondary: "var(--color-font-secondary)",
+        tertiary: "var(--color-font-tertiary)",
+      },
+      backgroundColor: {
+        neutral: "var(--color-bg-neutral)",
+      },
+      borderRadius: {
+        primary: "var(--radius-primary)",
+      },
+      backgroundImage: {
+        "hero-background": "url('/hero-banner.webp')",
+      },
+      screens: {
+        sm: "375px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1440px",
+        "2xl": "1920px",
+        "3xl": "2560px",
       },
     },
   },
